792382faf2ec28d9b2144700edb0f167803c899a
Lilian Arago <lilian@nahis-wayard.fr>
2018-08-26 16:37:46 +0200

    Edited ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       host : ['nahis-wayard.fr'],
       ref  : 'origin/master',
       repo : 'git@github.com:NahisWayard/web-chat.git',
-      path : '~/web-chat',
+      path : '/home/deployer/web-chat',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
